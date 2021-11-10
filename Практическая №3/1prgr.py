e05d006b7b503f154f4b300054b00162b3b3a57a
Iv1n <90642026+Iv1n@users.noreply.github.com>
2021-11-10 17:57:41 +0300

    Add files via upload

--- Практическая №3/1prgr.py
+++ Практическая №3/1prgr.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-x = int(input("Введите первое число :"))
-y = int(input("Введите второе число :"))
-if x <= y:
- def schet():
-	 print("Вывод :")
-	 for i in range(x, y + 1):
-		 print(i)
- print(schet())
-else:
-	print("Поменяйте второе число на первое")
+def prgr1():
+    x = int(input("Введите первое число :"))
+    y = int(input("Введите второе число :"))
+    if x <= y:
+      def schet():
+	      print("Вывод :")
+	      for i in range(x, y + 1):
+	    	  print(i)
+      print(schet())
+    else:
+	    print("Поменяйте второе число на первое")
+print (prgr1())
