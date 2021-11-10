fea70b8fae40f5ed7925ba10742259c753a90c10
Iv1n <90642026+Iv1n@users.noreply.github.com>
2021-11-10 18:33:09 +0300

    Add files via upload

--- Практическая №3/2prgr.py
+++ Практическая №3/2prgr.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
-x = int(input("Введите первое число :"))
-y = int(input("Введите второе число :"))
-if x < y:
- def schet():
-	 print("Вывод :")
-	 for i in range(x, y + 1):
-		 print(i)
- print(schet())
-else:
-	def sh():
-		for i in range(x, y - 1, -1):
-			print(i)
-	print(sh())		
+def program2():
+    x = int(input("Введите первое число :"))
+    y = int(input("Введите второе число :"))
+    if x < y:
+     def schet():
+	     print("Вывод :")
+	     for i in range(x, y + 1):
+		     print(i)
+     print(schet())
+    else:
+	    def sh():
+		    for i in range(x, y - 1, -1):
+			    print(i)
+	    print(sh())	
+print(program2())
--- Практическая №3/3prgr.py
+++ Практическая №3/3prgr.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
-x = int(input("Введите первое число :"))
-y = int(input("Введите второе число :"))
-if x > y:
- def schet():
-	 print("Вывод :")
-	 for i in range(x, y - 1, -1):
-		 if (i % 2 !=0):
-		  print(i)
- print(schet())
-else:
-	print("Поменяйте второе число на первое")	
+def program3():
+    x = int(input("Введите первое число :"))
+    y = int(input("Введите второе число :"))
+    if x > y:
+     def schet():
+	     print("Вывод :")
+	     for i in range(x, y - 1, -1):
+		     if (i % 2 !=0):
+		      print(i)
+     print(schet())
+    else:
+     print("Поменяйте второе число на первое")	
+print(program3())
