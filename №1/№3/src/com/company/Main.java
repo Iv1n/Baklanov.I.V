8b9fe03af0b744c4f404026b56efaeb61b0d8aee
Iv1n <90642026+Iv1n@users.noreply.github.com>
2022-02-12 18:18:49 +0300

    Add files via upload

---
+++ №1/№3/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	if(args.length == 2){
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int result = a+b;
+
+        System.out.println(a + "+" + b + "=" + result);
+    }
+    else{
+        System.out.println("неверное количество параметров");
+    }
+    }
+}
